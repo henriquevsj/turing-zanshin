32761e87726e54d24cf8cf7beb672ad44bbcaed6
henrique_vsj <henrique.vieira2008@hotmail.com>
2024-05-02 19:39:59 -0300

    Flask to FastAPI

--- api/webhook-form/main.py
+++ api/webhook-form/main.py
@@ -1,23 +1,21 @@
-import sys
-sys.path.append('../microservices')
+from fastapi import FastAPI, Request
 import logging
 import os
-from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Conecte-se ao banco de dados MongoDB
-client = MongoClient(f"mongodb+srv://{os.environ.get("MONGO_USER_NAME")}:{os.environ.get('MONGO_PASSWORD')}@cluster01.it0lbcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
+client = MongoClient(f"mongodb+srv://{os.environ.get('MONGO_USER_NAME')}:{os.environ.get('MONGO_PASSWORD')}@cluster01.it0lbcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
 db = client['doconnect_db']  # Altere 'mydatabase' para o nome do seu banco de dados
 collection = db['tenchi_security']  # Altere 'projects' para o nome da sua coleção
 
 logging.basicConfig(level=logging.INFO)  # Configurando o nível de log para INFO
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
+@app.post("/webhook")
+async def webhook(request: Request):
     try:
-        analysis_elements = request.json  # Obtém os dados da requisição POST
+        analysis_elements = await request.json()  # Obtém os dados da requisição POST
 
         if analysis_elements:
             inserted_id = collection.insert_one(analysis_elements).inserted_id
@@ -32,14 +30,15 @@ def webhook():
             logging.info(f"Dados atualizados com sucesso para o documento com ID: {inserted_id}")
             
             response = {'message': 'Data inserted and updated successfully', 'inserted_id': str(inserted_id)}
-            return jsonify(response), 200
+            return response, 200
         else:
             logging.error("Nenhum dado recebido do webhook")  # Log para indicar que nenhum dado foi recebido
-            return jsonify({'error': 'No data received'}), 400
+            return {'error': 'No data received'}, 400
         
     except Exception as e:
         logging.exception(f"Erro ao processar o webhook: {e}")  # Log para indicar o erro ocorrido
-        return jsonify({'error': 'Internal server error'}), 500
+        return {'error': 'Internal server error'}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
