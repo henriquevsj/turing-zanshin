bec8845fa8795a2e90364cffdb8eb18b40fc0741
henrique_vsj <henrique.vieira2008@hotmail.com>
2024-05-08 11:58:17 -0300

    Change to decorators

--- webhook-form/app.py
+++ webhook-form/app.py
@@ -1,32 +1,28 @@
-from common.mongodb_connector import MongoDB
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 import logging
-import os
+from common.mongodb_connector import MongoDB
 import uvicorn
 
 app = FastAPI()
 
-logging.basicConfig(level=logging.INFO)  # Configurando o nível de log para INFO
+logging.basicConfig(level=logging.INFO)  
 
-@app.route("/")
-def home(request: Request):
+@app.get("/")
+async def home():
     return PlainTextResponse("Flask Vercel Example - Hello World", status_code=200)
 
 @app.post("/webhook")
 async def webhook(request: Request):
     try:
-        # Criando uma instância da classe MongoDB usando um gerenciador de contexto
         with MongoDB() as db_instance:
-            analysis_elements = await request.json()  # Obtém os dados da requisição POST
+            analysis_elements = await request.json()  
 
             if analysis_elements:
-                # Insere os dados no banco de dados usando a instância MongoDB
                 inserted_id = db_instance.db['tenchi_security'].insert_one(analysis_elements).inserted_id
                     
                 logging.info(f"ID do documento: {inserted_id}")
 
-                # Atualiza o documento recém-inserido com os dados específicos
                 update_query = {'_id': inserted_id}
                 update_data = {'$set': {'AnalysisElements': analysis_elements}}
                 db_instance.db['tenchi_security'].update_one(update_query, update_data)
@@ -37,11 +33,11 @@ async def webhook(request: Request):
                 return response, 200
             
             else:
-                logging.error("Nenhum dado recebido do webhook")  # Log para indicar que nenhum dado foi recebido
+                logging.error("Nenhum dado recebido do webhook")  
                 return {'error': 'No data received'}, 400
             
     except Exception as e:
-        logging.exception(f"Erro ao processar o webhook: {e}")  # Log para indicar o erro ocorrido
+        logging.exception(f"Erro ao processar o webhook: {e}")  
         return {'error': 'Internal server error'}, 500
 
 if __name__ == '__main__':
