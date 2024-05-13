8fd7424837bb7a64d0badc6930665ec436053ede
henrique_vsj <henrique.vieira2008@hotmail.com>
2024-05-13 13:44:33 -0300

    Fix errors and implement a Azure Functions timer_trigger

--- cspm/zanshin/main.py
+++ cspm/zanshin/main.py
@@ -1,9 +1,47 @@
 import logging
 import requests
+import azure.functions as func
 from common.mongodb_fetch_data import DataFetcher
 from common.mongodb_update_document import UpdateDocument
 
 logging.basicConfig(level=logging.INFO)  # Configurando o nível de log para INFO
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="http_trigger1")
+def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+@app.timer_trigger(schedule="0 */10 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+
+def timer_trigger1(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+    logging.info('Python timer trigger function executed.')
+
+    fetch_data = DataFetcher()
+    try:
+        fetch_data.fetch_data(process_document)
+    except Exception as e:
+        logging.info(f"Erro ao buscar dados: {e}")
 
 def process_document(db_instance, collection, document):
     # Obter as informações necessárias do documento
@@ -69,10 +107,3 @@ def process_response(response_data, project_name, tool_type, tool_name, zanshin_
             selected_fields.append(_filter) 
 
     return selected_fields
-
-if __name__ == "__main__":
-    fetch_data = DataFetcher()
-    try:
-        fetch_data.fetch_data(process_document)
-    except Exception as e:
-        print(f"Erro ao buscar dados: {e}")
