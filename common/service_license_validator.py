a9f4758e3d4a12c657dd9851301099c04b845cfa
henrique_vsj <henrique.vieira2008@hotmail.com>
2024-06-27 16:22:19 -0300

    Update license validator

--- common/service_license_validator.py
+++ common/service_license_validator.py
@@ -1,47 +1,50 @@
 import logging
-import jwt
-import datetime
 import os
-import azure.functions as func
+import requests
 
 class LicenseValidator:
-    def __init__(self):      
-        # Definindo a chave secreta usada para assinar os tokens
-        self.secret_key = "Ainda assim, existem dúvidas a respeito de como o julgamento imparcial das eventualidades ainda não demonstrou convincentemente que vai participar na mudança dos paradigmas corporativos."
+    def __init__(self):
+        self.license_service_login_url = os.getenv('LICENSE_SERVICE_LOGIN_URL')
+        self.license_service_verify_url = os.getenv('LICENSE_SERVICE_VERIFY_URL')
+        self.username = os.getenv('LICENSE_SERVICE_USERNAME')
+        self.password = os.getenv('LICENSE_SERVICE_PASSWORD')
+        self.token = None
+
+    def get_token(self):
+        try:
+            response = requests.post(self.license_service_login_url, json={
+                "username": self.username,
+                "password": self.password
+            })
+
+            if response.status_code == 200:
+                self.token = response.json().get('token')
+                if self.token:
+                    return True
+                else:
+                    logging.error("Token not found in the response")
+                    return False
+            else:
+                logging.error(f"Failed to login: {response.text}")
+                return False
+        except Exception as e:
+            logging.error(f"Error during login: {e}")
+            return False
 
     def verify_license(self):
-        # Obtendo o token do cabeçalho da solicitação
-        token = os.getenv('TURING_LICENSE_TOKEN')
-        logging.info(f"Token: {token}")
-        
-        try:
-            if not token or not token.startswith("Bearer "):
-                logging.info("Token is missing or invalid format", status_code=401)
-                return func.HttpResponse("Token is missing or invalid format", status_code=401)
-        except Exception as e:
-            logging.error(f"oken is missing or invalid format: {e}")
-            return False
-            
-        try:
-            # Remover o prefixo "Bearer " do token, caso esteja presente
-            if token.startswith("Bearer "):
-                token = token.split(" ")[1]
-
-            decoded_token = jwt.decode(token, self.secret_key, algorithms=["HS256"])
-            logging.info(f"Token decodificado: {decoded_token}")
-            
-            expiration = decoded_token.get('exp')
-            if expiration and datetime.datetime.fromtimestamp(expiration, datetime.timezone.utc) < datetime.datetime.now(datetime.timezone.utc):
-                logging.error("License has expired.")
+        if not self.token:
+            if not self.get_token():
                 return False
-            return True
-        
-        except jwt.ExpiredSignatureError:
-            logging.error("License token has expired.")
-            return False
-        except jwt.InvalidTokenError as e:
-            logging.error(f"Invalid license token: {e}")
-            return False
+
+        try:
+            headers = {"Authorization": f"Bearer {self.token}"}
+            response = requests.post(self.license_service_verify_url, headers=headers, json={"token": self.token})
+
+            if response.status_code == 200:
+                return True
+            else:
+                logging.error(f"License validation failed: {response.text}")
+                return False
         except Exception as e:
-            logging.error(f"Erro ao verificar o token: {e}")
+            logging.error(f"Error verifying the token: {e}")
             return False
